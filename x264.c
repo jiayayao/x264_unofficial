80c4028ce05318ba2c1a03c0408d8ece03d03c04
jiayayao <jiayayao@126.com>
2017-06-22 14:02:34 +0800

    使用最原始的命令行调试编码过程
    Signed-off-by: jiayayao <jiayayao@126.com>

--- x264.c
+++ x264.c
@@ -349,188 +349,191 @@ static void print_version_info( void )
 #endif
 }
 
-//int main( int argc, char **argv )
-//{
-//    x264_param_t param;
-//    cli_opt_t opt = {0};
-//    int ret = 0;
-//
-//    FAIL_IF_ERROR( x264_threading_init(), "unable to initialize threading\n" );
-//
-//#ifdef _WIN32
-//    FAIL_IF_ERROR( !get_argv_utf8( &argc, &argv ), "unable to convert command line to UTF-8\n" );
-//
-//    GetConsoleTitleW( org_console_title, CONSOLE_TITLE_SIZE );
-//    _setmode( _fileno( stdin ),  _O_BINARY );
-//    _setmode( _fileno( stdout ), _O_BINARY );
-//    _setmode( _fileno( stderr ), _O_BINARY );
-//#endif
-//
-//    /* Parse command line */
-//    if( parse( argc, argv, &param, &opt ) < 0 )
-//        ret = -1;
-//
-//#ifdef _WIN32
-//    /* Restore title; it can be changed by input modules */
-//    SetConsoleTitleW( org_console_title );
-//#endif
-//
-//    /* Control-C handler */
-//    signal( SIGINT, sigint_handler );
-//
-//    if( !ret )
-//        ret = encode( &param, &opt );
-//
-//    /* clean up handles */
-//    if( filter.free )
-//        filter.free( opt.hin );
-//    else if( opt.hin )
-//        cli_input.close_file( opt.hin );
-//    if( opt.hout )
-//        cli_output.close_file( opt.hout, 0, 0 );
-//    if( opt.tcfile_out )
-//        fclose( opt.tcfile_out );
-//    if( opt.qpfile )
-//        fclose( opt.qpfile );
-//
-//#ifdef _WIN32
-//    SetConsoleTitleW( org_console_title );
-//    free( argv );
-//#endif
-//
-//    return ret;
-//}
 
-int main(int argc, char** argv)
+int main( int argc, char **argv )
 {
-	int ret;
-	int y_size;
-	int i, j;
+    x264_param_t param;
+    cli_opt_t opt = {0};
+    int ret = 0;
 
-	//FILE* fp_src  = fopen("../cuc_ieschool_640x360_yuv444p.yuv", "rb");
-	FILE* fp_src = fopen("../cuc_ieschool_640x360_yuv420p.yuv", "rb");
+	MessageBox(NULL, L"Please attach!", (L"Attach"), NULL);
 
-	FILE* fp_dst = fopen("cuc_ieschool.h264", "wb");
+    FAIL_IF_ERROR( x264_threading_init(), "unable to initialize threading\n" );
 
-	//Encode 50 frame
-	//if set 0, encode all frame
-	int frame_num = 50;
-	int csp = X264_CSP_I420;
-	int width = 640, height = 360;
+#ifdef _WIN32
+    FAIL_IF_ERROR( !get_argv_utf8( &argc, &argv ), "unable to convert command line to UTF-8\n" );
 
-	int iNal = 0;
-	x264_nal_t* pNals = NULL;
-	x264_t* pHandle = NULL;
-	x264_picture_t* pPic_in = (x264_picture_t*)malloc(sizeof(x264_picture_t));
-	x264_picture_t* pPic_out = (x264_picture_t*)malloc(sizeof(x264_picture_t));
-	x264_param_t* pParam = (x264_param_t*)malloc(sizeof(x264_param_t));
+    GetConsoleTitleW( org_console_title, CONSOLE_TITLE_SIZE );
+    _setmode( _fileno( stdin ),  _O_BINARY );
+    _setmode( _fileno( stdout ), _O_BINARY );
+    _setmode( _fileno( stderr ), _O_BINARY );
+#endif
 
-	//Check
-	if (fp_src == NULL || fp_dst == NULL) {
-		printf("Error open files.\n");
-		return -1;
-	}
+    /* Parse command line */
+    if( parse( argc, argv, &param, &opt ) < 0 )
+        ret = -1;
 
-	x264_param_default(pParam);
-	pParam->i_width = width;
-	pParam->i_height = height;
-	/*
-	//Param
-	pParam->i_log_level  = X264_LOG_DEBUG;
-	pParam->i_threads  = X264_SYNC_LOOKAHEAD_AUTO;
-	pParam->i_frame_total = 0;
-	pParam->i_keyint_max = 10;
-	pParam->i_bframe  = 5;
-	pParam->b_open_gop  = 0;
-	pParam->i_bframe_pyramid = 0;
-	pParam->rc.i_qp_constant=0;
-	pParam->rc.i_qp_max=0;
-	pParam->rc.i_qp_min=0;
-	pParam->i_bframe_adaptive = X264_B_ADAPT_TRELLIS;
-	pParam->i_fps_den  = 1;
-	pParam->i_fps_num  = 25;
-	pParam->i_timebase_den = pParam->i_fps_num;
-	pParam->i_timebase_num = pParam->i_fps_den;
-	*/
-	pParam->i_csp = csp;
-	x264_param_apply_profile(pParam, x264_profile_names[5]);
+#ifdef _WIN32
+    /* Restore title; it can be changed by input modules */
+    SetConsoleTitleW( org_console_title );
+#endif
 
-	pHandle = x264_encoder_open(pParam);
+    /* Control-C handler */
+    signal( SIGINT, sigint_handler );
 
-	x264_picture_init(pPic_out);
-	x264_picture_alloc(pPic_in, csp, pParam->i_width, pParam->i_height);
+    if( !ret )
+        ret = encode( &param, &opt );
 
-	//ret = x264_encoder_headers(pHandle, &pNals, &iNal);
+    /* clean up handles */
+    if( filter.free )
+        filter.free( opt.hin );
+    else if( opt.hin )
+        cli_input.close_file( opt.hin );
+    if( opt.hout )
+        cli_output.close_file( opt.hout, 0, 0 );
+    if( opt.tcfile_out )
+        fclose( opt.tcfile_out );
+    if( opt.qpfile )
+        fclose( opt.qpfile );
 
-	y_size = pParam->i_width * pParam->i_height;
-	//detect frame number
-	if (frame_num == 0) {
-		fseek(fp_src, 0, SEEK_END);
-		switch (csp) {
-		case X264_CSP_I444:frame_num = ftell(fp_src) / (y_size * 3); break;
-		case X264_CSP_I420:frame_num = ftell(fp_src) / (y_size * 3 / 2); break;
-		default:printf("Colorspace Not Support.\n"); return -1;
-		}
-		fseek(fp_src, 0, SEEK_SET);
-	}
+#ifdef _WIN32
+    SetConsoleTitleW( org_console_title );
+    free( argv );
+#endif
 
-	//Loop to Encode
-	for (i = 0; i<frame_num; i++) {
-		switch (csp) {
-		case X264_CSP_I444: {
-			fread(pPic_in->img.plane[0], y_size, 1, fp_src);	//Y
-			fread(pPic_in->img.plane[1], y_size, 1, fp_src);	//U
-			fread(pPic_in->img.plane[2], y_size, 1, fp_src);	//V
-			break; }
-		case X264_CSP_I420: {
-			fread(pPic_in->img.plane[0], y_size, 1, fp_src);	//Y
-			fread(pPic_in->img.plane[1], y_size / 4, 1, fp_src);	//U
-			fread(pPic_in->img.plane[2], y_size / 4, 1, fp_src);	//V
-			break; }
-		default: {
-			printf("Colorspace Not Support.\n");
-			return -1; }
-		}
-		pPic_in->i_pts = i;
-
-		ret = x264_encoder_encode(pHandle, &pNals, &iNal, pPic_in, pPic_out);
-		if (ret< 0) {
-			printf("Error.\n");
-			return -1;
-		}
-
-		printf("Succeed encode frame: %5d\n", i);
-
-		for (j = 0; j < iNal; ++j) {
-			fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
-		}
-	}
-	i = 0;
-	//flush encoder
-	while (1) {
-		ret = x264_encoder_encode(pHandle, &pNals, &iNal, NULL, pPic_out);
-		if (ret == 0) {
-			break;
-		}
-		printf("Flush 1 frame.\n");
-		for (j = 0; j < iNal; ++j) {
-			fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
-		}
-		i++;
-	}
-	x264_picture_clean(pPic_in);
-	x264_encoder_close(pHandle);
-	pHandle = NULL;
-
-	free(pPic_in);
-	free(pPic_out);
-	free(pParam);
-
-	fclose(fp_src);
-	fclose(fp_dst);
-
-	return 0;
+    return ret;
 }
+
+//int main(int argc, char** argv)
+//{
+//	int ret;
+//	int y_size;
+//	int i, j;
+//
+//	//FILE* fp_src  = fopen("../cuc_ieschool_640x360_yuv444p.yuv", "rb");
+//	FILE* fp_src = fopen("../cuc_ieschool_640x360_yuv420p.yuv", "rb");
+//
+//	FILE* fp_dst = fopen("cuc_ieschool.h264", "wb");
+//
+//	//Encode 50 frame
+//	//if set 0, encode all frame
+//	int frame_num = 50;
+//	int csp = X264_CSP_I420;
+//	int width = 640, height = 360;
+//
+//	int iNal = 0;
+//	x264_nal_t* pNals = NULL;
+//	x264_t* pHandle = NULL;
+//	x264_picture_t* pPic_in = (x264_picture_t*)malloc(sizeof(x264_picture_t));
+//	x264_picture_t* pPic_out = (x264_picture_t*)malloc(sizeof(x264_picture_t));
+//	x264_param_t* pParam = (x264_param_t*)malloc(sizeof(x264_param_t));
+//
+//	//Check
+//	if (fp_src == NULL || fp_dst == NULL) {
+//		printf("Error open files.\n");
+//		return -1;
+//	}
+//
+//	x264_param_default(pParam);
+//	pParam->i_width = width;
+//	pParam->i_height = height;
+//	/*
+//	//Param
+//	pParam->i_log_level  = X264_LOG_DEBUG;
+//	pParam->i_threads  = X264_SYNC_LOOKAHEAD_AUTO;
+//	pParam->i_frame_total = 0;
+//	pParam->i_keyint_max = 10;
+//	pParam->i_bframe  = 5;
+//	pParam->b_open_gop  = 0;
+//	pParam->i_bframe_pyramid = 0;
+//	pParam->rc.i_qp_constant=0;
+//	pParam->rc.i_qp_max=0;
+//	pParam->rc.i_qp_min=0;
+//	pParam->i_bframe_adaptive = X264_B_ADAPT_TRELLIS;
+//	pParam->i_fps_den  = 1;
+//	pParam->i_fps_num  = 25;
+//	pParam->i_timebase_den = pParam->i_fps_num;
+//	pParam->i_timebase_num = pParam->i_fps_den;
+//	*/
+//	pParam->i_csp = csp;
+//	x264_param_apply_profile(pParam, x264_profile_names[5]);
+//
+//	pHandle = x264_encoder_open(pParam);
+//
+//	x264_picture_init(pPic_out);
+//	ret = x264_picture_alloc(pPic_in, csp, pParam->i_width, pParam->i_height);
+//
+//	//ret = x264_encoder_headers(pHandle, &pNals, &iNal);
+//
+//	y_size = pParam->i_width * pParam->i_height;
+//	//detect frame number
+//	if (frame_num == 0) {
+//		fseek(fp_src, 0, SEEK_END);
+//		switch (csp) {
+//		case X264_CSP_I444:frame_num = ftell(fp_src) / (y_size * 3); break;
+//		case X264_CSP_I420:frame_num = ftell(fp_src) / (y_size * 3 / 2); break;
+//		default:printf("Colorspace Not Support.\n"); return -1;
+//		}
+//		fseek(fp_src, 0, SEEK_SET);
+//	}
+//
+//	//Loop to Encode
+//	for (i = 0; i<frame_num; i++) {
+//		switch (csp) {
+//		case X264_CSP_I444: {
+//			fread(pPic_in->img.plane[0], y_size, 1, fp_src);	//Y
+//			fread(pPic_in->img.plane[1], y_size, 1, fp_src);	//U
+//			fread(pPic_in->img.plane[2], y_size, 1, fp_src);	//V
+//			break; }
+//		case X264_CSP_I420: {
+//			fread(pPic_in->img.plane[0], y_size, 1, fp_src);	//Y
+//			fread(pPic_in->img.plane[1], y_size / 4, 1, fp_src);	//U
+//			fread(pPic_in->img.plane[2], y_size / 4, 1, fp_src);	//V
+//			break; }
+//		default: {
+//			printf("Colorspace Not Support.\n");
+//			return -1; }
+//		}
+//		pPic_in->i_pts = i;
+//
+//		ret = x264_encoder_encode(pHandle, &pNals, &iNal, pPic_in, pPic_out);
+//		if (ret< 0) {
+//			printf("Error.\n");
+//			return -1;
+//		}
+//
+//		printf("Succeed encode frame: %5d\n", i);
+//
+//		for (j = 0; j < iNal; ++j) {
+//			fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
+//		}
+//	}
+//	i = 0;
+//	//flush encoder
+//	while (1) {
+//		ret = x264_encoder_encode(pHandle, &pNals, &iNal, NULL, pPic_out);
+//		if (ret == 0) {
+//			break;
+//		}
+//		printf("Flush 1 frame.\n");
+//		for (j = 0; j < iNal; ++j) {
+//			fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
+//		}
+//		i++;
+//	}
+//	x264_picture_clean(pPic_in);
+//	x264_encoder_close(pHandle);
+//	pHandle = NULL;
+//
+//	free(pPic_in);
+//	free(pPic_out);
+//	free(pParam);
+//
+//	fclose(fp_src);
+//	fclose(fp_dst);
+//
+//	return 0;
+//}
 
 static char const *strtable_lookup( const char * const table[], int idx )
 {
@@ -1539,6 +1542,7 @@ static int parse( int argc, char **argv, x264_param_t *param, cli_opt_t *opt )
             break;
         }
 
+		// 解析参数列表
         switch( c )
         {
             case 'h':
